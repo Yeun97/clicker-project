648fc2fcd69f4bf3f31949de53cc2ceec29c92d0
Yeun97 <36974116+Yeun97@users.noreply.github.com>
2021-04-23 14:11:58 -0400

    lambda function to get messages

---
+++ readMessage.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+
+exports.handler = async (event, context, callback) => {
+    await readMessage().then(data => {
+        data.Items.forEach(function(item){
+            console.log(item.message)
+        });
+        callback(null, {
+            statusCode: 200,
+            body: data.Items,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+        })
+    }).catch((err) => {
+        console.error(err);
+    })
+};
+
+function readMessage() {
+    
+    const params = {
+        TableName: 'Message',
+        IndexName: 'game-date-index'
+    }
+    
+    return ddb.scan(params).promise();
+}
